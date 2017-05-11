2b701be0096b0a7874058235a9d5fcc91139e619
emrahyldrm <emrahyildirim.ist@gmail.com>
2017-05-11 09:04:18 +0000

    Routes updated

--- classifier_app/config/routes.rb
+++ classifier_app/config/routes.rb
@@ -1,25 +1,27 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
-  get 'users/new'
+  root   'static_pages#home'
 
-	root 'static_pages#home'
+  get    'sessions/new'
+  get    'users/new'
 
-  get  '/create',  to: 'classifier_request#create'
-  get  '/result',  to: 'classifier_request#result'
-  post '/create',  to: 'classifier_request#create'
-
+  get    '/create',  to: 'classifier_request#create'
+  post   '/create',  to: 'classifier_request#create'
   get    '/request', to: 'classifier_request#req'
-  get    '/help',    to: 'static_pages#help'
-  get    '/about',   to: 'static_pages#about'
-  get    '/contact', to: 'static_pages#contact'
+  get    '/result',  to: 'classifier_request#result'
+
+
+  get    '/help',     to: 'static_pages#help'
+  get    '/about',    to: 'static_pages#about'
+  get    '/contact',  to: 'static_pages#contact'
   get    '/download', to: 'static_pages#down'
-  get    '/signup',  to: 'users#new'
-  post   '/signup',  to: 'users#create'
-  post   '/del',  to: 'users#del'
-  get    '/login',   to: 'sessions#new'
-  post   '/login',   to: 'sessions#create'
-  delete '/logout',  to: 'sessions#destroy'
+  get    '/signup',   to: 'users#new'
+  post   '/signup',   to: 'users#create'
+  post   '/del',      to: 'users#del'
+  get    '/login',    to: 'sessions#new'
+  post   '/login',    to: 'sessions#create'
+  delete '/logout',   to: 'sessions#destroy'
+
   resources :users
 
 end
